dcc294f7c1707a3c80610f624b050afeefc5acd5
cheesecake <farzanaman99@gmail.com>
2026-09-04 10:05:20 +0000

    eee

---
+++ misc/partial_send_handling.c
@@ -0,0 +1,22 @@
+//a little function to handle Partial send() s and make sure no data is left in the buffer
+
+#include<sys/types.h>
+#include<sys/socket.h>
+
+int sendall(int s, char *buf, int *len)
+{
+	int total = 0;  //how many bytes are sent
+	int bytesleft = *len; //bytes left to send
+	int n;
+
+	while(total < *len) {
+		n = send(s, buf+total, bytesleft, 0);
+		if (n==-1) {break;}
+		total += n;
+		bytesleft -= n;
+	}
+
+	*len = total; //returning number actually sent
+		      
+	return n==-1?-1:0 //-1 if fail, 0 if success
+} 
